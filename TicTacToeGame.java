2da3cc6428f1ccd927857d8429c2de2477fcf9dd
Minal <patilminalp@gmail.com>
2021-06-21 03:36:27 +0530

    Added:using array and  ignore 0th position

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,10 +1,19 @@
-package com.program;
-
-public class TicTacToeGame{
-public static void main(String[] args){
+package com.program;public class TicTacToeGame {
+	private static char[] element;
 	
-	System.out.println("Welcome in Tic-Toc-Toe Game....!!!");
+	public static void main(String[] args) {
+		System.out.println("Welcome to Tic Tac Toe Game....!!!");
+		board();
+	}
 	
-}
-
+   	/*Create a board of char[] of size 10
+	and assign empty space to each
+	element*/
+	
+	private static void board() {
+		element = new char[10];
+		for(int i=1; i<10; i++) {
+			element[i]='.';
+		}
+	}
 }
