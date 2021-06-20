aaafda0f95e468ac4644c6db4eb77285ac74b547
Minal <patilminalp@gmail.com>
2021-06-21 03:54:34 +0530

    Added player to choose a letter X and O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,13 @@
-package com.program;public class TicTacToeGame {
+package com.program;
+import java.util.Scanner;
+
+public class TicTacToeGame {
 	private static char[] element;
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tac Toe Game....!!!");
+		System.out.println("Welcome to Tic Tac Toe Game");
 		board();
+		userinput();
 	}
 	
    	/*Create a board of char[] of size 10
@@ -15,5 +19,21 @@ package com.program;public class TicTacToeGame {
 		for(int i=1; i<10; i++) {
 			element[i]='.';
 		}
+	}
+	private static void userinput() {
+		 Scanner in=new Scanner(System.in);
+		  
+		  //Mark Selection
+		  System.out.println(" Select Your Marker (O or X): ");
+		  
+		 char usermark=in.next().charAt(0);
+		  char computermark;
+		  if (usermark=='X') {
+		  computermark='O';
+		  System.out.println("User mark is :" +usermark);}
+		  
+		  else
+		   computermark='X';
+		  System.out.println("User mark is :" +usermark);
 	}
 }
